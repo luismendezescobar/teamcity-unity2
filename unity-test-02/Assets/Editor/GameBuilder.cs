cacfabf698154d38510e83498017893df1b63f3a
Luis Mendez <luismendezescobar@gmail.com>
2024-05-27 20:41:36 -0600

    adding game builder class

---
+++ unity-test-02/Assets/Editor/GameBuilder.cs
@@ -0,0 +1,54 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+public class GameBuilder : MonoBehaviour
+{
+    [MenuItem("Build/Build macOS")]
+    public static void PerformMacOSBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "build/macOS/somegame.app" ;
+        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result==BuildResult.Succeeded)
+        {
+            Debug.Log(message: "Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log(message: "Build failed");
+        }
+    }
+
+    [MenuItem("Build/Build WebGL")]
+    public static void PerformWebGLBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "build/WebGL";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log(message: "Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log(message: "Build failed");
+        }
+    }
+
+
+}
